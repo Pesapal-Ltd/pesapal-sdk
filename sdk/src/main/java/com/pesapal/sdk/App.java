8b3de7a0f2e787e90a8b33457094ac94a93718ec
Richard <richard@pesapal.com>
2023-04-17 15:29:22 +0300

    SDK-23 #IN PROGRESS - code optimization in progress

--- sdk/src/main/java/com/pesapal/sdk/App.java
+++ sdk/src/main/java/com/pesapal/sdk/App.java
@@ -12,13 +12,6 @@ public class App extends Application {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
     }
 
-    public static App getContext() {
-        if (mInstance == null) {
-            mInstance = new App();
-        }
-        return mInstance;
-    }
-
     public static synchronized App getInstance() {
         if (mInstance == null) {
             mInstance = new App();
